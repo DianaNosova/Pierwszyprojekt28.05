5153a0e987b1ef7665bcb7fdae28796d4ea40e0e
Diana Nosova <nosova.diana1997@gmail.com>
2023-05-28 14:04:25 +0200

    First test implemented

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -1,7 +1,11 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UltimateQATest extends PageSetup {
 
@@ -14,6 +18,12 @@ public class UltimateQATest extends PageSetup {
 
     @Test
     public void testOne() {
+        WebElement buttonUsingId = driver.findElementById( "idExample");
+        buttonUsingId.click();
+
+
+        WebElement buttonSuccessText =driver.findElementByClassName("entry-title");
+        assertEquals("Button success", buttonSuccessText.getText());
 
     }
 
