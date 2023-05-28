e810d7ac6ad28ae51a492cbd1143b0da17e73baf
Diana Nosova <nosova.diana1997@gmail.com>
2023-05-28 16:42:58 +0200

    Tests implemented

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -5,7 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UltimateQATest extends PageSetup {
 
@@ -48,7 +52,40 @@ public class UltimateQATest extends PageSetup {
 
      }
 
+     @Test
+    public void testThree() {
+         WebElement blueField = driver.findElementByXPath("//*[contains(@class, 'et_pb_module et_pb_cta_0 et_pb_promo')]");
 
+        assertEquals("rgba(46, 163, 242, 1)", blueField.getCssValue("background-color"));
+
+     }
+
+     @Test
+    public void  testFour(){
+
+     }
+
+     @Test
+    public void testFive(){
+        List<String> listOfCars=new ArrayList<>();
+
+        listOfCars.add("Volvo");
+        listOfCars.add("Saab");
+        listOfCars.add("Opel");
+        listOfCars.add("Audi");
+
+        for (int i = 0; i < listOfCars.size(); i++)
+        {
+            WebElement dropdownOption = driver.findElementByXPath("//*[@value='" + listOfCars.get(i).toLowerCase() + "']");
+            dropdownOption.click();
+            assertTrue(dropdownOption.isSelected());
+        }
+
+        
+
+
+
+    }
 }
 
 
